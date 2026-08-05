d581d4be7b36274480ee04737ac44eaf3be40e31
Dominic Taylor <dominic.jam.taylor@gmail.com>
2026-08-05 04:23:49 -0400

    Created initial login screen UI and Continuum Theme

--- Continuum/app/src/main/java/com/continuum/MainActivity.kt
+++ Continuum/app/src/main/java/com/continuum/MainActivity.kt
@@ -5,43 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.continuum.screens.LoginScreen
 import com.continuum.ui.theme.ContinuumTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             ContinuumTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    LoginScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ContinuumTheme {
-        Greeting("Android")
     }
 }
--- Continuum/app/src/main/java/com/continuum/ui/theme/Color.kt
+++ Continuum/app/src/main/java/com/continuum/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.continuum.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary Brand Colors
+val BluePrimary = Color(0xFF2563EB)
+val CyanAccent = Color(0xFF06B6D4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Backgrounds
+val NavyBackground = Color(0xFF0F172A)
+val Surface = Color(0xFF111827)
+
+// Text
+val PrimaryText = Color(0xFFF8FAFC)
+val MutedText = Color(0xFF94A3B8)
+
+// Status Colors
+val Success = Color(0xFF22C55E)
+val Warning = Color(0xFFF59E0B)
+val Critical = Color(0xFFEF4444)
+
+// UI
+val Border = Color(0xFF475569)
