ea67f78f7e73d7abcc7d510cc9a09b0ac8d5a845
Dominic Taylor <dominic.jam.taylor@gmail.com>
2026-08-17 17:28:24 -0400

    Add records page and connect screen navigation

--- Continuum/app/src/main/java/com/continuum/Navigation.kt
+++ Continuum/app/src/main/java/com/continuum/Navigation.kt
@@ -4,49 +4,94 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.continuum.screens.CreateHandoffScreen
 import com.continuum.screens.HomeScreen
 import com.continuum.screens.LoginScreen
+import com.continuum.screens.RecordsScreen
 import com.continuum.screens.RegisterScreen
 import kotlinx.serialization.Serializable
 
 @Serializable
 object Login
+
 @Serializable
 object Register
+
 @Serializable
 object Home
 
+@Serializable
+object NewHandoff
+
+@Serializable
+object Records
+
 @Composable
-fun Navigate(db : Database) {
+fun Navigate(db: Database) {
     val navController = rememberNavController()
 
-    // TODO Make sure startDestination is set back to Login
-    NavHost(navController = navController, startDestination = Login) {
+    NavHost(
+        navController = navController,
+        startDestination = Login
+    ) {
+
         composable<Login> {
             LoginScreen(
                 db,
-                toRegister = { navController.navigate(route = Register) },
-                toHome = { navController.navigate(route = Home) {
-                    popUpTo(Login) {
-                        inclusive = true
+                toRegister = {
+                    navController.navigate(route = Register)
+                },
+                toHome = {
+                    navController.navigate(route = Home) {
+                        popUpTo(Login) {
+                            inclusive = true
+                        }
                     }
-                }}
+                }
             )
         }
+
         composable<Register> {
             RegisterScreen(
                 db,
-                toLogin = { navController.popBackStack() },
-                toHome = { navController.navigate(route = Home) {
-                    popUpTo(Login) {
-                        inclusive = true
+                toLogin = {
+                    navController.popBackStack()
+                },
+                toHome = {
+                    navController.navigate(route = Home) {
+                        popUpTo(Login) {
+                            inclusive = true
+                        }
                     }
-                }}
+                }
             )
         }
+
         composable<Home> {
             HomeScreen(
-                db,
+                db = db,
+                toNewHandoff = {
+                    navController.navigate(NewHandoff)
+                },
+                toHistory = {
+                    navController.navigate(Records)
+                }
+            )
+        }
+
+        composable<NewHandoff> {
+            CreateHandoffScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable<Records> {
+            RecordsScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                }
             )
         }
     }
